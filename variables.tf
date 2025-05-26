5d6ca388276c97226a5e2aae646f2c00e2407cdf
krishna-yadav1 <krishan.yadav@slovink.com>
2025-05-26 10:09:23 +0530

    fix subnet

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "subnet_names" {
   description = "The names of the subnets."
 }
 
-variable "network" {
+variable "network_id" {
   type        = string
   default     = ""
   description = "The VPC network the subnets belong to."
